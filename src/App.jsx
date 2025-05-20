00449624674479b2a203099b18c9248040789665
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-20 18:01:13 +0200

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,46 @@
-import React, { useState, useEffect, useMemo, memo } from "react";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [suggestion, setSuggestion] = useState([]);
+
+  const fetchProducts = async (query) => {
+    if (!query.trim()) {
+      setSuggestion([]);
+      return;
+    }
+
+    try {
+      const res = await fetch(`http://localhost:3333/products?search=${query}`);
+      const data = await res.json();
+      setSuggestion(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(query);
+  }, [query]);
+
+  return (
+    <div>
+      <h1>Autocomplete</h1>
+      <input
+        type="text"
+        placeholder="cerca un prodotto..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {suggestion.length > 0 && (
+        <div className="dropdown">
+          {suggestion.map((product) => (
+            <p key={product.id}>{product.name}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default App;
